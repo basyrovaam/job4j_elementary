1e8ad37a012199b955e5ec93a50dd839a4131c9c
basyrovaam <basyrovaam@gmail.com>
2020-07-27 19:02:13 +0300

    9.1. String [#326947]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -4,6 +4,7 @@ public class ArgMethod {
     public static void hello(String name) {
         System.out.println("Hello, " + name);
     }
+
     public static void main(String[] args) {
         String name = "Albina Basyrova";
         ArgMethod.hello(name);
