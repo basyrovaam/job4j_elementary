f96dbd50c95d75b93e3d9e0cdd44283c1c53c39d
basyrovaam <basyrovaam@gmail.com>
2020-07-27 19:11:19 +0300

    9.2. Checkstyle [#326952]

--- src/ru/job4j/StudentInf.java
+++ src/ru/job4j/StudentInf.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInf {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Albina Basyrova");
         System.out.println("29.11.1993");
             }
