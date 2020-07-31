2166e2936f84392fa48fd849e3cce76ad334325c
basyrovaam <basyrovaam@gmail.com>
2020-07-31 13:43:34 +0300

    изменила путь.

--- src/ru/job4j/condition/StudentInf.java
+++ src/ru/job4j/condition/StudentInf.java
@@ -4,5 +4,5 @@ public class StudentInf {
     public static void main(String[] args) {
         System.out.println("Albina Basyrova");
         System.out.println("29.11.1993");
-            }
+    }
 }
