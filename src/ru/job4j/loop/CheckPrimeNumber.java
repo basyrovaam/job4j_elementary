266ba03e54e6d338a98913a68616c751868e78b6
basyrovaam <basyrovaam@gmail.com>
2020-07-31 15:44:48 +0300

    5.5. Простое число [#326994]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number == 1) {
+            prime = false;
+        }
+        for (int index = 2; index < number; index++) {
+            if (number % index == 0) {
+                prime = false;
+                break;
+            }
+
+            }
+        return prime;
+    }
+
+        public static void main(String[] args) {
+            System.out.println(check(5));
+            System.out.println(check(4));
+            System.out.println(check(1));
+            System.out.println(check(2));
+        }
+    }
