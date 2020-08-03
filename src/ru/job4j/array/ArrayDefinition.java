a015ce4deb53bf7a6fe7d1db6b9fcb911011e7ab
basyrovaam <basyrovaam@gmail.com>
2020-08-03 23:21:36 +0300

    6.0.2. Заполнение массива. [#326922]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Albina Basyrova";
+        names[2] = "Olga Petrova";
+        names[3] = "Ekaterina Rudaya";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
